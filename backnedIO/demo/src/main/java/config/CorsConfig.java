5dee477aba3c5ba52794e00e78386b650860a904
aloix123 <pawel.kupis@o2.pl>
2025-10-22 23:44:19 +0200

    feat: adding cors

--- backnedIO/demo/src/main/java/config/CorsConfig.java
+++ backnedIO/demo/src/main/java/config/CorsConfig.java
@@ -3,10 +3,12 @@ package config;
 // CorsConfig.java
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin(origins = "http://3.69.167.48:8080")
 public class CorsConfig {
 
     @Bean
