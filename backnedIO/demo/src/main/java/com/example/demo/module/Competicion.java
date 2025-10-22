cdff82f724e9ccd7cb0ca69d2cb269883cc4d001
aloix123 <pawel.kupis@o2.pl>
2025-10-22 23:31:30 +0200

    feat: adding cors

--- backnedIO/demo/src/main/java/com/example/demo/module/Competicion.java
+++ backnedIO/demo/src/main/java/com/example/demo/module/Competicion.java
@@ -5,8 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
