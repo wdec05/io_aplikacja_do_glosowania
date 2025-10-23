03075df5e21a3b736df902ff89afc1514a08eed3
aloix123 <pawel.kupis@o2.pl>
2025-10-23 13:12:29 +0200

    feat: adding tasks really fast for Paweł

--- backnedIO/demo/src/main/java/com/example/demo/module/Competicion.java
+++ backnedIO/demo/src/main/java/com/example/demo/module/Competicion.java
@@ -1,11 +1,10 @@
 package com.example.demo.module;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -15,13 +14,22 @@ public class Competicion {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-    private String emailsthat_voted;
+    @ElementCollection
+    private List<Long> emailsthat_voted;
 
     Competicion(){}
 
-    public Competicion(String name, String emailsthat_voted) {
+    public Competicion(String name, List<Long> emailsthat_voted) {
         this.name = name;
         this.emailsthat_voted = emailsthat_voted;
     }
+    public Competicion(String name){
+        this.name = name;
+    }
 
+    //return all competicions that exists in database
+    // return all projects by competition ID
+    // create competition endpint
+    // crate Project
+    //
 }
---
+++ backnedIO/demo/src/main/java/com/example/demo/module/Project.java
@@ -0,0 +1,34 @@
+package com.example.demo.module;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long competicionId;
+    private String name;
+    private String title;
+    private String description;
+    private byte[] image;
+    private long votes;
+
+    public Project(){}
+
+    public Project(String title, String description, byte[] image, long votes, long competicionId) {
+        this.competicionId = competicionId;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.votes = votes;
+    }
+}
